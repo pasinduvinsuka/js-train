dc106a14aee2f8a57190fde4bc8d0c40a62f33c8
Pasindu Vinsuka <88902687+pasinduvinsuka@users.noreply.github.com>
2023-04-18 15:51:24 +0530

    created constructor and added fuctions

---
+++ ex1/ex1.js
@@ -0,0 +1,33 @@
+class book {
+        title;
+        author;
+        ISBN;
+        amount;
+        soldAmount;
+    constructor(title, author, ISBN, amount) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.amount = amount;
+    }
+}
+
+function booksAvailability() {
+
+    if ( amount < 10){
+        console.log('low stock');
+    }else if (amount == 0){
+        console.log('out of stock');
+    }else{
+        console.log('in stock');
+    }
+}
+
+function sellBook(name, soldAmount) {
+    this.soldAmount = soldAmount;
+    let remain = book1.amount - soldAmount;
+    console.log(`new amount is ${remain}`)
+}
+
+let book1 = new book('blue ocean ','Mark Adam','783237XXX','100');
+sellBook(20);
