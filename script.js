dd06c449d11abed44e2b95d9cc7a04fa1b4f2d04
Pasindu Vinsuka <88902687+pasinduvinsuka@users.noreply.github.com>
2023-04-18 14:29:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ for (let i = 0; i < text.length; i++){
     text[i].style.fontWeight = 'bold';
     text[i].style.color = 'green';
     text[i].style.fontSize = '15px';
+
+   
+
+    var textx = text[i].textContent;
+    console.log(textx);
     
 }
