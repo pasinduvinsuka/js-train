e0612b5ed07115a0a1c0bd93b418f248bb7481ce
Pasindu Vinsuka <88902687+pasinduvinsuka@users.noreply.github.com>
2023-04-18 14:17:02 +0530

    getElementByClassName
    In here this will return a HTML collection.

---
+++ script.js
@@ -0,0 +1,7 @@
+let text = document.getElementsByClassName('text');
+for (let i = 0; i < text.length; i++){
+    text[i].style.fontWeight = 'bold';
+    text[i].style.color = 'green';
+    text[i].style.fontSize = '15px';
+    
+}
